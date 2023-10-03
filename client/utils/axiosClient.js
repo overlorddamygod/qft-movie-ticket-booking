a1cb5054426b7c285a295fce2c4177ce51ec6852
Overlord <overlord.damygod@gmail.com>
2023-10-03 11:47:34 +0545

    fix: client https

--- client/utils/axiosClient.js
+++ client/utils/axiosClient.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import https from "https";
 
 const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
 });
 
 axiosClient.interceptors.request.use(function (config) {
@@ -24,6 +28,9 @@ const ssrAxiosClient = axios.create({
   baseURL: isDevelopment
     ? process.env.NEXT_PUBLIC_SSR_SERVER_URL
     : process.env.NEXT_PUBLIC_SSR_SERVER_URL,
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false
+    })
 });
 
 export { ssrAxiosClient };
